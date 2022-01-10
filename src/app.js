b835598487e4b2ded53289d1e51c35c42442a05a
Dmitry Menshikov <dimaldo86ne@mail.ru>
2022-01-11 02:09:52 +0700

    Eslint configuration

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const a = 1;
+function foo(name) {
+  const lastName = name;
+  return lastName;
+}
+foo('Dima')
+let x = 200
+// eslint-disable-next-line no-plusplus
+x++;
+
+console.log(window);
+// eslint-disable-next-line no-console
+console.log(x, a);
+// nmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+const arrow = (y) => {
+	return y + 1		
+}
+
+const obj = {
+	name:'Dima',
+}
+const name = obj.name
+
+
+
+
+
